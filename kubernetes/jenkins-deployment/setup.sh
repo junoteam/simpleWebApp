c3da704f722461e8efcf4c32e50b55c07738434d
Alex Berber <berber.it@gmail.com>
2020-04-07 20:48:51 +0300

    setup.sh configured. plan adde

--- kubernetes/jenkins-deployment/setup.sh
+++ kubernetes/jenkins-deployment/setup.sh
@@ -1,13 +1,23 @@
+#!/bin/bash 
+# GKE only setup
+
+echo "Creating attachable volume in Google cloud"
 gcloud compute disks create --size 20GB kubernetes-jenkins
 
+echo "Generating private key and cert"
 ssh-keygen -t rsa -b 4096 -C "jenkins" -f jenkins-ssh -q -N ""
-GITHUB_IP=$(getent hosts github.com | awk '{ print $1 }')
+
+echo "Setup of known-hosts file"
+GITHUB_IP=$(host github.com | awk '{ print $4 ; exit 1}')
 ssh-keyscan -t rsa -T 10 github.com >> known-hosts
 ssh-keyscan -t rsa -T 10 $GITHUB_IP >> known-hosts
-getent hosts github.com | awk '{ print $1 }'
-kubectl create secret generic jenkins-ssh --from-file=jenkins-ssh --from-file=jenkins-ssh.pub --from-file=known-hosts --from-file="config=ssh_config"
-rm jenkins-ssh*
-rm known-hosts
 
-kubectl create -f jenkins-deployment.yaml
-kubectl create -f jenkins-service.yaml
+echo "Creating Kube secret"
+kubectl create secret generic jenkins-ssh --from-file=jenkins-ssh --from-file=jenkins-ssh.pub --from-file=known-hosts --from-file="config=ssh_config"
+
+echo "Cleaning out"
+rm -v jenkins-ssh*
+rm -v known-hosts
+
+echo "Creating Jenkins deployment in Kube"
+kubectl create -f jenkins-deployment.yml
