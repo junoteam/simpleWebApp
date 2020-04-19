f5d58759e3f605517b4dd355e3144912846a635a
Alex Berber <berber.it@gmail.com>
2020-04-19 11:43:18 +0300

    another route added

--- simpleWebApp.go
+++ simpleWebApp.go
@@ -1,24 +1,31 @@
 package main
 
 import (
-        "net/http"
-        "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-        r := gin.Default()
-        r.LoadHTMLGlob("templates/*")
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 
-		r.GET("/", handleIndex)
-		r.GET("/ping", handlePing)
-		r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080
+	r.GET("/", handleIndex)
+	r.GET("/ping", handlePing)
+	r.GET("/health", handleHealth)
+	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080
 }
 
 func handleIndex(c *gin.Context) {
-        c.HTML(http.StatusOK, "simpleweb.tmpl", gin.H{})
+	c.HTML(http.StatusOK, "simpleweb.tmpl", gin.H{})
 }
 
 func handlePing(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-		"message": "pong",})
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong"})
+}
+
+func handleHealth(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "healthy"})
 }
